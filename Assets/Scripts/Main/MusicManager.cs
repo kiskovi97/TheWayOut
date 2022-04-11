6b38a4f828f5066ad139dc01e2219765e646041e
Kovács Gergő <gergo.kovacs@bookrkids.hu>
2022-04-11 15:02:36 +0200

    Music and advertismenet

---
+++ Assets/Scripts/Main/MusicManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Main
+{
+    class MusicManager : MonoBehaviour
+    {
+        private static MusicManager Instance { get; set; }
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        void OnDestroy()
+        {
+            if (Instance == this)
+            {
+                Instance = null;
+            }
+        }
+    }
+}
